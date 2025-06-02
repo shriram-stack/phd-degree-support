1230a5edb5a2d7faf37e3fbc32028149d20d29f2
shriram.k <bughunter230@gmail.com>
2025-06-02 16:56:37 +0530

    Create scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,4 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    // Form submission is handled by Web3Forms; this is just for feedback
+    alert('Thank you for your message! We will get back to you soon.');
+});
